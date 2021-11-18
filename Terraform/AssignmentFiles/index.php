44df1ca66d0a548ed64c5fdee70456460973ebef
tplex99 <61918309+tplex99@users.noreply.github.com>
2021-11-18 11:47:06 +0100

    test

--- Terraform/AssignmentFiles/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-        echo file_get_contents('https://xlq5hokesc.execute-api.eu-west-1.amazonaws.com/default/test');
-        //lambda endpoint
-
-?>
-<br>
-<img src="https://tmcloud.s3-eu-west-1.amazonaws.com/image.png">
-<!--image file stored on S3-->
